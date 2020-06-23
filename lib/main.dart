bc62093f71c03f17be9c91a7b26b1a8a4dcc8274
CesarMtzV <mc.cesar.art@gmail.com>
2020-06-23 14:01:14 -0500

    -Changed the drawer header to a UserAccountsDrawerHeader -Added icons to. each. drawer list element -Changed most of the code to a separate vault.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:password_manager/vault.dart';
 
 void main() {
   runApp(TabBarDemo());
@@ -11,93 +12,10 @@ class TabBarDemo extends StatefulWidget {
 
 class _TabBarDemoState extends State<TabBarDemo> {
 
-  bool isSearching = false;
-
   @override
   Widget build(BuildContext context) {
-
-    final _TabPages = <Widget>[
-      Center(child: Text("Page 1"),),
-      Center(child: Text("Page 2"),),
-      Center(child: Text("Page 3"),),
-    ];
-
-    final _Tabs = <Tab>[
-      Tab(text: "ALL",),
-      Tab(text: "RECENT",),
-      Tab(text: "FAVORITES",),
-    ];
-
-    final _ListTiles = <Widget>[
-      DrawerHeader(
-        child: Text("Drawer header"),
-        decoration: BoxDecoration(
-          color: Colors.blue,
-        ),
-      ),
-      ListTile(
-        title: Text("Item 1"),
-        onTap: () {
-          print("Item 1 selected");
-        },
-      ),
-      ListTile(
-        title: Text("Item 2"),
-        onTap: () {
-          print("Item 2 selected");
-        },
-      )
-    ];
-
     return MaterialApp(
-      home: DefaultTabController(
-        length: _Tabs.length,
-        child: Scaffold(
-          appBar: AppBar(
-            bottom: TabBar(
-              tabs: _Tabs,
-            ),
-            title: !isSearching 
-            ? Text("Titulo") 
-            : TextField(
-              decoration: InputDecoration(
-                hintText: "Search here",
-                hintStyle: TextStyle(color: Colors.grey),
-                icon: Icon(Icons.search),
-              ),
-            ),
-            backgroundColor: Colors.black54,
-            actions: <Widget>[
-              isSearching 
-              ? IconButton(
-                  icon: Icon(Icons.cancel),
-                  onPressed: () {
-                    setState(() {
-                      this.isSearching = false;
-                    });
-                  },
-                )
-              : IconButton(
-                  icon: Icon(Icons.search),
-                  onPressed: () {
-                    setState(() {
-                      this.isSearching = true;
-                    });
-                  },
-                ),
-            ],
-          ),
-          body: TabBarView(
-            children: _TabPages,
-          ),
-          drawer: Drawer(
-            child: ListView(
-              padding: EdgeInsets.zero,
-              children: _ListTiles,
-            ),
-          ),
-        ),
-      ),
+      home: Vault(),
     );
   }
 }
---
+++ lib/vault.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+class Vault extends StatefulWidget {
+  Vault({Key key}) : super(key: key);
+
+  _VaultState createState() => _VaultState();
+}
+
+class _VaultState extends State<Vault> {
+
+  bool isSearching = false;
+
+  @override
+  Widget build(BuildContext context) {
+
+    final _TabPages = <Widget>[
+      Center(child: Text("Page 1"),),
+      Center(child: Text("Page 2"),),
+      Center(child: Text("Page 3"),),
+    ];
+
+    final _Tabs = <Tab>[
+      Tab(text: "ALL",),
+      Tab(text: "RECENT",),
+      Tab(text: "FAVORITES",),
+    ];
+
+    final _ListTiles = <Widget>[
+      UserAccountsDrawerHeader(
+        accountName: Text("Cesar Martinez"),
+        accountEmail: Text("mc.cesar.art@gmail.com"),
+        currentAccountPicture: CircleAvatar(
+          backgroundColor: Colors.grey[900],
+          child: Text(
+            'C', 
+            style: TextStyle(
+              fontSize: 40.0
+            ),
+          ),
+        ),
+      ),
+      ListTile(
+        title: Text("Vault"),
+        leading: Icon(Icons.lock),
+        onTap: () {
+          print("Vault selected");
+        },
+      ),
+      ListTile(
+        title: Text("Settings"),
+        leading: Icon(Icons.settings),
+        onTap: () {
+          print("Settings selected");
+        },
+      ),
+      ListTile(
+        title: Text("Sign out"),
+        leading: Icon(Icons.exit_to_app),
+        onTap: () {
+          print("Settings selected");
+        },
+      ),
+    ];
+
+    return DefaultTabController(
+        length: _Tabs.length,
+        child: Scaffold(
+          backgroundColor: Colors.grey[900],
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: _Tabs,
+            ),
+            title: !isSearching 
+            ? Text("Titulo") 
+            : TextField(
+              decoration: InputDecoration(
+                hintText: "Search here",
+                hintStyle: TextStyle(color: Colors.grey),
+                icon: Icon(Icons.search),
+              ),
+            ),
+            backgroundColor: Colors.grey[850],
+            actions: <Widget>[
+              isSearching 
+              ? IconButton(
+                  icon: Icon(Icons.cancel),
+                  onPressed: () {
+                    setState(() {
+                      this.isSearching = false;
+                    });
+                  },
+                )
+              : IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () {
+                    setState(() {
+                      this.isSearching = true;
+                    });
+                  },
+                ),
+            ],
+          ),
+          body: TabBarView(
+            children: _TabPages,
+          ),
+          drawer: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: _ListTiles,
+            ),
+          ),
+        ),
+      );
+  }
+}
