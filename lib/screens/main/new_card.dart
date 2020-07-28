35d22497f0a15b27a65ef500d321a91fa55be199
Alex Vargas <alexjesusvargas@gmail.com>
2020-07-28 17:36:21 -0500

    fixed bug in new_card.dart

--- lib/screens/main/new_card.dart
+++ lib/screens/main/new_card.dart
@@ -16,68 +16,71 @@ class NewCard extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: Container(
+        height: 700,
         color: Color(0xFF212121),
         padding: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                labelText: 'Email',
-                labelStyle: TextStyle(color: Colors.white),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  labelStyle: TextStyle(color: Colors.white),
+                ),
+                controller: emailController,
               ),
-              controller: emailController,
-            ),
-            TextField(
-              style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                labelText: 'Username',
-                labelStyle: TextStyle(color: Colors.white),
+              TextField(
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                  labelStyle: TextStyle(color: Colors.white),
+                ),
+                controller: userController,
               ),
-              controller: userController,
-            ),
-            TextField(
-              keyboardType: TextInputType.url,
-              style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                labelText: 'Password',
-                labelStyle: TextStyle(color: Colors.white),
+              TextField(
+                keyboardType: TextInputType.url,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  labelStyle: TextStyle(color: Colors.white),
+                ),
+                controller: passwordController,
               ),
-              controller: passwordController,
-            ),
-            TextField(
-              keyboardType: TextInputType.url,
-              style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                labelText: 'URL',
-                labelStyle: TextStyle(color: Colors.white),
+              TextField(
+                keyboardType: TextInputType.url,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  labelText: 'URL',
+                  labelStyle: TextStyle(color: Colors.white),
+                ),
+                controller: urlController,
               ),
-              controller: urlController,
-            ),
-            TextField(
-              style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                labelText: 'Site Name',
-                labelStyle: TextStyle(color: Colors.white),
+              TextField(
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  labelText: 'Site Name',
+                  labelStyle: TextStyle(color: Colors.white),
+                ),
+                controller: siteNameController,
               ),
-              controller: siteNameController,
-            ),
-            FlatButton(
-              child: Text('Add'),
-              textColor: Color(0xFFC62828),
-              onPressed: () {
-                addCard(
-                  emailController.text,
-                  userController.text,
-                  passwordController.text,
-                  urlController.text,
-                  siteNameController.text,
-                );
-              },
-            ),
-          ],
+              FlatButton(
+                child: Text('Add'),
+                textColor: Color(0xFFC62828),
+                onPressed: () {
+                  addCard(
+                    emailController.text,
+                    userController.text,
+                    passwordController.text,
+                    urlController.text,
+                    siteNameController.text,
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
