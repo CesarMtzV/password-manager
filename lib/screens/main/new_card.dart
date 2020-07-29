79c533ec1d5f410e5da51fbd1345a884f69bf2c2
Alex Vargas <alexjesusvargas@gmail.com>
2020-07-29 18:05:02 -0500

    new_card.dart email validators

--- lib/screens/main/new_card.dart
+++ lib/screens/main/new_card.dart
@@ -11,6 +11,23 @@ class NewCard extends StatelessWidget {
 
   NewCard(this.addCard);
 
+  final _formKey = GlobalKey<FormState>();
+
+  void _submit() {
+    final isValid = _formKey.currentState.validate();
+
+    if (isValid) {
+      _formKey.currentState.save();
+      addCard(
+        emailController.text,
+        userController.text,
+        passwordController.text,
+        urlController.text,
+        siteNameController.text,
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,61 +40,74 @@ class NewCard extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.end,
             children: <Widget>[
-              TextField(
-                keyboardType: TextInputType.emailAddress,
-                style: TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  labelStyle: TextStyle(color: Colors.white),
+              Form(
+                key: _formKey,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                        //style
+                        keyboardType: TextInputType.emailAddress,
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          labelStyle: TextStyle(color: Colors.white),
+                        ),
+                        //controller
+                        controller: emailController,
+                        //validation
+                        validator: (input) {
+                          if (input.isEmpty || !input.contains('@')) {
+                            return 'Please enter a valid email';
+                          } else {
+                            return null;
+                          }
+                        },
+                      ),
+                      TextFormField(
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          labelText: 'Username',
+                          labelStyle: TextStyle(color: Colors.white),
+                        ),
+                        controller: userController,
+                      ),
+                      TextFormField(
+                        keyboardType: TextInputType.url,
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          labelStyle: TextStyle(color: Colors.white),
+                        ),
+                        controller: passwordController,
+                        obscureText: true,
+                      ),
+                      TextFormField(
+                        keyboardType: TextInputType.url,
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          labelText: 'URL',
+                          labelStyle: TextStyle(color: Colors.white),
+                        ),
+                        controller: urlController,
+                      ),
+                      TextFormField(
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          labelText: 'Site Name',
+                          labelStyle: TextStyle(color: Colors.white),
+                        ),
+                        controller: siteNameController,
+                      ),
+                      FlatButton(
+                        child: Text('Add'),
+                        textColor: Color(0xFFC62828),
+                        onPressed: _submit,
+                      ),
+                    ],
+                  ),
                 ),
-                controller: emailController,
-              ),
-              TextField(
-                style: TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                  labelText: 'Username',
-                  labelStyle: TextStyle(color: Colors.white),
-                ),
-                controller: userController,
-              ),
-              TextField(
-                keyboardType: TextInputType.url,
-                style: TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  labelStyle: TextStyle(color: Colors.white),
-                ),
-                controller: passwordController,
-              ),
-              TextField(
-                keyboardType: TextInputType.url,
-                style: TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                  labelText: 'URL',
-                  labelStyle: TextStyle(color: Colors.white),
-                ),
-                controller: urlController,
-              ),
-              TextField(
-                style: TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                  labelText: 'Site Name',
-                  labelStyle: TextStyle(color: Colors.white),
-                ),
-                controller: siteNameController,
-              ),
-              FlatButton(
-                child: Text('Add'),
-                textColor: Color(0xFFC62828),
-                onPressed: () {
-                  addCard(
-                    emailController.text,
-                    userController.text,
-                    passwordController.text,
-                    urlController.text,
-                    siteNameController.text,
-                  );
-                },
               ),
             ],
           ),
